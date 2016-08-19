f07641b391f154e270f5891369e0018a0523fc9a
DavidC <david@origamica.com>
2016-08-19 11:37:11 +0200

    logger with test

--- test/test-logger.js
+++ test/test-logger.js
@@ -3,24 +3,25 @@ import sinon from 'sinon';
 import Rx from 'rxjs';
 import logger from '../src/logger';
 
-test.only('logger', t => {
+test('logger', t => {
   const dataLog = [
     { streamName: 'someStream', payload: 'some payload' },
     { streamName: 'someOtherStream', payload: 'some other payload' },
     { streamName: 'someStream3', payload: 'payload3' }
   ];
   const dataWant = [
-    'someStream: some payload',
-    'someOtherStream: some other payload',
-    'someStream3: payload3'
+    [ 'someStream: ', 'some payload' ],
+    [ 'someOtherStream: ', 'some other payload' ],
+    [ 'someStream3: ', 'payload3' ]
   ];
 
   const streamToMonitorS = new Rx.Subject;
-  sinon.spy(console.log);
+  sinon.spy(console, 'log');
   logger(streamToMonitorS);
   t.plan(3);
   dataLog.forEach((val, index) => {
     streamToMonitorS.next(val);
-    t.true(toLog.called()); // TODO calledWith dataWant[index]
+    t.true(console.log.calledWith(...dataWant[index])); // eslint-disable-line
   });
+  console.log.restore(); // eslint-disable-line
 });
